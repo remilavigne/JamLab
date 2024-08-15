a27e39022c76072e162f3acbd0fe3b2634a01dea
Remi Lavigne <remilavigne.pro@gmail.com>
2024-08-15 15:42:20 +0200

    Fixed <img> on hero section with <Image />

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -76,46 +76,56 @@ export default function Home() {
                 <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                   <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                     <div className="relative">
-                      <img
+                      <Image
                         alt="Band on stage"
-                        src="/unsplash_KVB3PzQ8sFo.png"
+                        src="/unsplash_YaYcCW1nlqc.png"
                         className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                        width={200}
+                        height={200}
                       />
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-neutral-900/10" />
                     </div>
                   </div>
                   <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
                     <div className="relative">
-                      <img
+                      <Image
                         alt="Singer playing banjo"
                         src="/unsplash_RxpqZBBevKg.png"
                         className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                        width={200}
+                        height={200}
                       />
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-neutral-900/10" />
                     </div>
                     <div className="relative">
-                      <img
+                      <Image
                         alt="Band on stage with lights"
                         src="/unsplash_sqJ4tLBiurw.png"
-                        className="aspect-[2/3] w-full rounded-xl bg-neutral-900/5 object-cover shadow-lg"
+                        className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                        width={200}
+                        height={200}
                       />
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-neutral-900/10" />
                     </div>
                   </div>
                   <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
                     <div className="relative">
-                      <img
-                        alt="Guitarist on stage"
+                      <Image
+                          alt="Guitarist on stage"
                         src="/unsplash_qE851OTuYIk.png"
-                        className="aspect-[2/3] w-full rounded-xl bg-neutral-900/5 object-cover shadow-lg"
+                        className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                        width={200}
+                        height={200}
                       />
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-neutral-900/10" />
                     </div>
                     <div className="relative">
-                      <img
-                        alt="Rock band on stage"
+                      <Image
+                         alt="Rock band on stage"
                         src="/unsplash__uDj_lyPVpA.png"
-                        className="aspect-[2/3] w-full rounded-xl bg-neutral-900/5 object-cover shadow-lg"
+                        className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                        width={200}
+                        height={200}
                       />
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-neutral-900/10" />
                     </div>

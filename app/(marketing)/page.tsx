4a6e8873a2d200d992289f66f75019fe70185bba
Remi Lavigne <remilavigne.pro@gmail.com>
2024-08-21 11:13:02 +0200

    Css updated on Home, sjgn-in and sign-up page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -46,34 +46,34 @@ export default function Home() {
                 clipPath:
                   'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
               }}
-              className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+              className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#f97316] to-[#22d3ee] opacity-30"
             />
           </div>
           <div className="overflow-hidden">
             <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
               <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                 <div className="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-                  <h1 className="text-4xl font-bold tracking-tight text-neutral-900 sm:text-6xl">
+                  <h1 className="text-6xl lg:text-8xl font-raleway font-bold tracking-tight text-neutral-900 ">
                     Band together.<br></br>
                     Play Better.
                   </h1>
                   <p className="mt-6 text-lg leading-8 text-neutral-700 sm:max-w-md lg:max-w-none">
                     An intuitive platform for music groups who want to streamline their workflow and maximize their creativity.
                   </p>
-                  <div className="mt-10 flex items-center gap-x-6">
+                  <div className="mt-10 flex items-center flex-col lg:flex-row gap-6 ">
                     <Links
                       href="/sign-up"
-                      className="rounded-md bg-orange-400 px-3.5 py-2.5 text-sm font-semibold text-neutral-900 shadow-sm hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-400"
+                      className="w-full rounded-md bg-orange-400 px-3.5 py-2.5 text-base font-semibold text-center text-neutral-900 shadow-sm hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-400"
                     >
                       Register for free
                     </Links>
-                    <Links href="/sign-in" className="text-sm font-semibold leading-6 text-neutral-900"
+                    <Links href="/sign-in" className="w-full text-base font-semibold text-center leading-6 text-neutral-900"
                     >
                       Log in <span aria-hidden="true">→</span>
                     </Links>
                   </div>
                 </div>
-                <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+                <div className="mt-14 sm:mt-6 flex justify-end gap-8 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                   <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                     <div className="relative">
                       <Image
